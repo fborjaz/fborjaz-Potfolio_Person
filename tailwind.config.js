2f473d8392df28ccd25a05e42bbd2f9cf53ee194
fborjaz <fborjaz@unemi.edu.ec>
2024-08-06 20:32:12 -0500

    update: add button darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,34 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js'
   ],
+
+  // make sure to safelist these classes when using purge
+  safelist: [
+    'w-64',
+    'w-1/2',
+    'rounded-l-lg',
+    'rounded-r-lg',
+    'bg-gray-200',
+    'grid-cols-4',
+    'grid-cols-7',
+    'h-6',
+    'leading-6',
+    'h-9',
+    'leading-9',
+    'shadow-lg'
+  ],
+
+  // enable dark mode via class strategy
+  darkMode: 'class',
+
   theme: {
     extend: {},
   },
-  plugins: [],
+
+  plugins: [
+    // include Flowbite as a plugin in your Tailwind CSS project
+    require('flowbite/plugin')
+  ],
 }
